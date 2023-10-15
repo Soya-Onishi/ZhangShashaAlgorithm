67f5cb3d6a17855a504832fa5bf6bed52932584e
Soya Ohnishi <soya.ohnishi@gmail.com>
2023-10-15 14:17:35 +0900

    Add index setter in constructor

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,22 @@ use std::collections::{HashMap, HashSet};
 
 #[derive(Eq, Ord, Hash)]
 struct Node {
-    id: usize,
+    index: usize,
     content: String,
     children: Vec<Node>,
 }
 
 impl Node {
-    fn new<T: Into<String>>(id: usize, content: T, children: Vec<Node>) -> Node {
-        Node {
-            id,
+    fn new<T: Into<String>>(content: T, children: Vec<Node>) -> Node {
+        let mut node = Node {
+            index: 0,
             content: content.into(),
             children,
-        }
+        };
+
+        node.set_index(&mut 0);
+
+        node
     }
 
     fn depth_priority_vec(&self) -> Vec<&Node> {
@@ -31,19 +35,28 @@ impl Node {
     fn len(&self) -> usize {
         self.children.iter().map(|c| c.len()).sum::<usize>() + 1
     }
+
+    fn set_index(&mut self, base_index: &mut usize) {
+        self.index = *base_index;
+
+        for c in self.children.iter_mut() {
+            *base_index = *base_index + 1;
+            c.set_index(base_index)
+        }
+    }
 }
 
 impl PartialEq for Node {
     fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
+        self.index == other.index
     }
 }
 
 impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        let cmp = if self.id > other.id {
+        let cmp = if self.index > other.index {
             std::cmp::Ordering::Greater
-        } else if self.id < other.id {
+        } else if self.index < other.index {
             std::cmp::Ordering::Less
         } else {
             std::cmp::Ordering::Equal
@@ -88,38 +101,38 @@ fn ted(x: &Node, y: &Node) -> (Matrix<usize>, Matrix<usize>) {
 
     for k in x_keyroots.iter() {
         for l in y_keyroots.iter() {
-            D[rl(k).id + 1][rl(l).id + 1] = 0;
+            D[rl(k).index + 1][rl(l).index + 1] = 0;
             let k_tree = k.depth_priority_vec();
             let l_tree = l.depth_priority_vec();
             for i in k_tree.iter().rev() {
-                D[i.id][rl(l).id + 1] = D[i.id + 1][rl(l).id + 1] + 1;
+                D[i.index][rl(l).index + 1] = D[i.index + 1][rl(l).index + 1] + 1;
             }
 
             for j in l_tree.iter().rev() {
-                D[rl(k).id + 1][j.id] = D[rl(k).id + 1][j.id + 1] + 1;
+                D[rl(k).index + 1][j.index] = D[rl(k).index + 1][j.index + 1] + 1;
             }
 
             for i in k_tree.iter().rev() {
                 for j in l_tree.iter().rev() {
-                    if rl(i).id == rl(k).id && rl(j).id == rl(l).id {
+                    if rl(i).index == rl(k).index && rl(j).index == rl(l).index {
                         let costs = [
-                            D[i.id + 1][j.id] + 1,
-                            D[i.id][j.id + 1] + 1,
-                            D[i.id + 1][j.id + 1] + rep_cost(i, j),
+                            D[i.index + 1][j.index] + 1,
+                            D[i.index][j.index + 1] + 1,
+                            D[i.index + 1][j.index + 1] + rep_cost(i, j),
                         ];
 
                         let cost = costs.into_iter().min().unwrap();
-                        D[i.id][j.id] = cost;
-                        d[i.id][j.id] = cost;
+                        D[i.index][j.index] = cost;
+                        d[i.index][j.index] = cost;
                     } else {
                         let costs = [
-                            D[i.id + 1][j.id] + 1,
-                            D[i.id][j.id + 1] + 1,
-                            D[rl(i).id + 1][rl(j).id + 1] + d[i.id][j.id],
+                            D[i.index + 1][j.index] + 1,
+                            D[i.index][j.index + 1] + 1,
+                            D[rl(i).index + 1][rl(j).index + 1] + d[i.index][j.index],
                         ];
 
                         let cost = costs.into_iter().min().unwrap();
-                        D[i.id][j.id] = cost;
+                        D[i.index][j.index] = cost;
                     }
                 }
             }
@@ -135,8 +148,8 @@ fn keyroots(root: &Node) -> HashSet<&Node> {
     let nodes = root.depth_priority_vec();
     for &n in nodes.iter() {
         let rl_node = rl(n);
-        if !rls.contains(&rl_node.id) {
-            rls.insert(rl_node.id);
+        if !rls.contains(&rl_node.index) {
+            rls.insert(rl_node.index);
             krs.insert(n);
         }
     }
@@ -171,27 +184,27 @@ fn backtr<'a>(
 ) {
     let k_nodes = k.depth_priority_vec();
     let l_nodes = l.depth_priority_vec();
-    if k.id > 0 && l.id > 0 {
+    if k.index > 0 && l.index > 0 {
         for i in k_nodes.iter().rev() {
-            D[i.id][rl(l).id + 1] = D[i.id + 1][rl(l).id + 1] + 1;
+            D[i.index][rl(l).index + 1] = D[i.index + 1][rl(l).index + 1] + 1;
         }
 
         for j in l_nodes.iter().rev() {
-            D[rl(k).id + 1][j.id] = D[rl(k).id + 1][j.id + 1] + 1;
+            D[rl(k).index + 1][j.index] = D[rl(k).index + 1][j.index + 1] + 1;
         }
 
         for i in k_nodes.iter().rev() {
             for j in l_nodes.iter().rev() {
-                if rl(i).id == rl(k).id && rl(j).id == rl(l).id {
-                    D[i.id][j.id] = d[i.id][j.id] + D[rl(k).id + 1][rl(l).id + 1];
+                if rl(i).index == rl(k).index && rl(j).index == rl(l).index {
+                    D[i.index][j.index] = d[i.index][j.index] + D[rl(k).index + 1][rl(l).index + 1];
                 } else {
                     let costs = [
-                        D[i.id + 1][j.id] + 1,
-                        D[i.id][j.id + 1] + 1,
-                        D[rl(i).id + 1][rl(j).id + 1] + d[i.id][j.id],
+                        D[i.index + 1][j.index] + 1,
+                        D[i.index][j.index + 1] + 1,
+                        D[rl(i).index + 1][rl(j).index + 1] + d[i.index][j.index],
                     ];
 
-                    D[i.id][j.id] = costs.into_iter().min().unwrap();
+                    D[i.index][j.index] = costs.into_iter().min().unwrap();
                 }
             }
         }
@@ -205,37 +218,37 @@ fn backtr<'a>(
     let mut j_node = l_nodes.next();
 
     while let Some((i, j)) = i_node.zip(j_node) {
-        if rl(i).id == rl(k).id && rl(j).id == rl(l).id {
-            if D[i.id][j.id] == D[i.id + 1][j.id + 1] + rep_cost(i, j) {
+        if rl(i).index == rl(k).index && rl(j).index == rl(l).index {
+            if D[i.index][j.index] == D[i.index + 1][j.index + 1] + rep_cost(i, j) {
                 map.push(ChangeType::Update(i, j));
                 i_node = k_nodes.next();
                 j_node = l_nodes.next();
                 continue;
             }
         } else {
-            if D[i.id][j.id] == D[rl(i).id + 1][rl(j).id + 1] + d[i.id][j.id] {
+            if D[i.index][j.index] == D[rl(i).index + 1][rl(j).index + 1] + d[i.index][j.index] {
                 backtr(x, y, d, D, map, i, j);
                 let rli = rl(i);
                 let rlj = rl(j);
 
-                i_node = if rli.id == i.id {
+                i_node = if rli.index == i.index {
                     k_nodes.next()
                 } else {
-                    k_nodes.find(|n| n.id == rli.id)
+                    k_nodes.find(|n| n.index == rli.index)
                 };
-                j_node = if rlj.id == j.id {
+                j_node = if rlj.index == j.index {
                     l_nodes.next()
                 } else {
-                    l_nodes.find(|n| n.id == rlj.id)
+                    l_nodes.find(|n| n.index == rlj.index)
                 };
 
                 continue;
             }
         }
-        if D[i.id][j.id] == D[i.id + 1][j.id] + 1 {
+        if D[i.index][j.index] == D[i.index + 1][j.index] + 1 {
             i_node = k_nodes.next();
             continue;
-        } else if D[i.id][j.id] == D[i.id][j.id + 1] + 1 {
+        } else if D[i.index][j.index] == D[i.index][j.index + 1] + 1 {
             j_node = l_nodes.next();
             continue;
         }
@@ -248,43 +261,47 @@ mod test {
 
     fn x_tree() -> Node {
         Node::new(
-            0,
             "a",
             vec![
-                Node::new(
-                    1,
-                    "b",
-                    vec![Node::new(2, "c", vec![]), Node::new(3, "d", vec![])],
-                ),
-                Node::new(4, "e", vec![]),
+                Node::new("b", vec![Node::new("c", vec![]), Node::new("d", vec![])]),
+                Node::new("e", vec![]),
             ],
         )
     }
 
     fn y_tree() -> Node {
-        Node::new(0, "f", vec![Node::new(1, "g", vec![])])
+        Node::new("f", vec![Node::new("g", vec![])])
     }
 
     fn z_tree() -> Node {
         Node::new(
-            0,
             "a",
             vec![
-                Node::new(1, "c", vec![]),
-                Node::new(2, "d", vec![]),
-                Node::new(3, "e", vec![]),
+                Node::new("c", vec![]),
+                Node::new("d", vec![]),
+                Node::new("e", vec![]),
             ],
         )
+    }
+
+    #[test]
+    fn index_test() {
+        let x = x_tree();
+        assert_eq!(x.index, 0);
+        assert_eq!(x.children[0].index, 1);
+        assert_eq!(x.children[0].children[0].index, 2);
+        assert_eq!(x.children[0].children[1].index, 3);
+        assert_eq!(x.children[1].index, 4);
     }
 
     #[test]
     fn rl_test() {
         let x = x_tree();
 
-        assert_eq!(rl(&x).id, 4);
-        assert_eq!(rl(&x.children[0]).id, 3);
-        assert_eq!(rl(&x.children[0].children[0]).id, 2);
-        assert_eq!(rl(&x.children[0].children[1]).id, 3);
+        assert_eq!(rl(&x).index, 4);
+        assert_eq!(rl(&x.children[0]).index, 3);
+        assert_eq!(rl(&x.children[0].children[0]).index, 2);
+        assert_eq!(rl(&x.children[0].children[1]).index, 3);
     }
 
     #[test]
@@ -293,13 +310,13 @@ mod test {
         let y = y_tree();
 
         let krs = keyroots(&x);
-        let ids: Vec<usize> = krs.iter().map(|n| n.id).collect();
+        let ids: Vec<usize> = krs.iter().map(|n| n.index).collect();
         assert!(ids.contains(&0), "keyroot ids: {:?}", &ids);
         assert!(ids.contains(&1), "keyroot ids: {:?}", &ids);
         assert_eq!(ids.len(), 3);
 
         let krs = keyroots(&y);
-        let ids: Vec<usize> = krs.iter().map(|n| n.id).collect();
+        let ids: Vec<usize> = krs.iter().map(|n| n.index).collect();
 
         assert_eq!(ids.len(), 1);
     }
